83913a248e20ed39668005527921382bd24ddfa8
Francisco Santos <fjgsantos98@gmail.com>
2017-08-04 15:54:55 -0600

    Elfos

---
+++ Lab3/src/lab3/Elfos.java
@@ -0,0 +1,18 @@
+
+package lab3;
+
+
+public class Elfos extends Integrantes {
+ public int Attk=443;
+ public int Def=150;
+ public int Heal=335;
+
+    public Elfos() {
+        super();
+    }
+    
+    
+ 
+ 
+ 
+}
