53390f8674f787cacaf884bb252a1c6d5b39bbd3
unknown <albemako@yahoo.com>
2017-08-04 14:12:09 -0600

    bestias2

--- Lab3/src/lab3/Bestias.java
+++ Lab3/src/lab3/Bestias.java
@@ -10,5 +10,42 @@ package lab3;
  * @author Francisco Santos
  */
 public class Bestias {
+    public int Atck;
+    public int Defensa;
+    public int curacion;
+
+    public Bestias() {
+    }
+
+    public Bestias(int Atck, int Defensa, int curacion) {
+        this.Atck = Atck;
+        this.Defensa = Defensa;
+        this.curacion = curacion;
+    }
+
+    public int getAtck() {
+        return Atck;
+    }
+
+    public void setAtck(int Atck) {
+        this.Atck = Atck;
+    }
+
+    public int getDefensa() {
+        return Defensa;
+    }
+
+    public void setDefensa(int Defensa) {
+        this.Defensa = Defensa;
+    }
+
+    public int getCuracion() {
+        return curacion;
+    }
+
+    public void setCuracion(int curacion) {
+        this.curacion = curacion;
+    }
+    
     
 }
